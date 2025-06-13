a3908cad6122a2edfe6feb8e420386ac95bb34ad
Kseniya <eseniya.wi@gmail.com>
2025-06-13 18:01:39 +0300

    practice 8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+$(document).ready(function() {
+    let angle = 0;
+    function rotate() {
+        angle += 1; // скорость вращения (градусы)
+        $('#circleText').css('transform', 'rotate(' + angle + 'deg)');
+        requestAnimationFrame(rotate);
+    }
+    rotate();
+});
